78b36dcd11b3686930f2bdae958c8285c0444fb0
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2021-04-19 15:30:16 +0200

    Few improvements to test script

--- test/test.C
+++ test/test.C
@@ -13,6 +13,8 @@ void fccd_sweep() {
         auto c = new TCanvas(Form("ch%i", ch), "c");
         c->cd();
         std::vector<TH1D*> histos;
+
+        // in grid
         for (double fccd = 0.65; fccd <= 2.4; fccd += 0.05) {
 
             auto h = new TH1D(Form("channel %i, fccd = %.2f mm, dlf = %.2f", ch, fccd, dlf),
@@ -24,6 +26,25 @@ void fccd_sweep() {
             h->Draw("c same");
             histos.push_back(h);
         }
+        // out grid
+        for (double fccd = 0.625; fccd <= 2.4; fccd += 0.05) {
+
+            auto h = new TH1D(Form("channel %i, fccd = %.2f mm, dlf = %.2f", ch, fccd, dlf),
+                    Form("channel %i, fccd = 0.65:0.05:2.4 mm, dlf = %.2f", ch, dlf), 400, 0, 400);
+
+            for (int i = 1; i <= h->GetNbinsX(); ++i) {
+                h->SetBinContent(i, gerda::ar39_pdf(ch, h->GetBinCenter(i), fccd, dlf));
+            }
+            h->SetLineColor(kRed);
+            h->Draw("c same");
+            histos.push_back(h);
+        }
+
+        c->Update();
+        auto l = new TLine(40, 0, 40, gPad->GetUymax());
+        l->SetLineStyle(2);
+        l->Draw();
+
         c->SaveAs(Form("plots/fccd/ch%i.pdf", ch));
         delete c;
         for (auto h : histos) delete h;
@@ -39,10 +60,11 @@ void dlf_sweep() {
         auto c = new TCanvas(Form("ch%i", ch), "c");
         c->cd();
         std::vector<TH1D*> histos;
-        for (double dlf = 1; dlf >= 0; dlf -= 0.05) {
+        // on grid
+        for (double dlf = 0.1; dlf <= 1; dlf += 0.1) {
 
             auto h = new TH1D(Form("channel %i, fccd = %.2f mm, dlf = %.2f", ch, fccd, dlf),
-                    Form("channel %i, fccd = %.2f mm, dlf = 0:0.05:1", ch, fccd), 400, 0, 400);
+                    Form("channel %i, fccd = %.2f mm, dlf = 0.1:0.05:1", ch, fccd), 400, 0, 400);
 
             for (int i = 1; i <= h->GetNbinsX(); ++i) {
                 h->SetBinContent(i, gerda::ar39_pdf(ch, h->GetBinCenter(i), fccd, dlf));
@@ -50,6 +72,25 @@ void dlf_sweep() {
             h->Draw("c same");
             histos.push_back(h);
         }
+        // out grid
+        for (double dlf = 0.15; dlf <= 1; dlf += 0.1) {
+
+            auto h = new TH1D(Form("channel %i, fccd = %.2f mm, dlf = %.2f", ch, fccd, dlf),
+                    Form("channel %i, fccd = %.2f mm, dlf = 0.1:0.05:1", ch, fccd), 400, 0, 400);
+
+            for (int i = 1; i <= h->GetNbinsX(); ++i) {
+                h->SetBinContent(i, gerda::ar39_pdf(ch, h->GetBinCenter(i), fccd, dlf));
+            }
+            h->SetLineColor(kRed);
+            h->Draw("c same");
+            histos.push_back(h);
+        }
+
+        c->Update();
+        auto l = new TLine(40, 0, 40, gPad->GetUymax());
+        l->SetLineStyle(2);
+        l->Draw();
+
         c->SaveAs(Form("plots/dlf/ch%i.pdf", ch));
         delete c;
         for (auto h : histos) delete h;
@@ -62,4 +103,8 @@ void test() {
     gROOT->SetBatch();
     fccd_sweep();
     dlf_sweep();
+}
+
+void speedtest() {
+    // gerda::ar39_pdf(0, 134.15, 1.57, 0.22);
 }
